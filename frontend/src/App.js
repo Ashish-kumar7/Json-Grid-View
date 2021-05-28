b55e9407314a3cd26f75e6ba5b538d6a77c6b82c
ashish007kumar <ashishkumar357ak@gmail.com>
2021-05-28 20:02:06 +0530

    deleted

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,20 @@
-
-import './App.css';
-
+// import './App.css';
+import React, { useState ,useEffect } from 'react';
+import { AddURL } from './MyComponents/AddURL';
+import { AddJSON } from './MyComponents/AddJSON';
+import { Header } from './MyComponents/Header';
+import { About } from './MyComponents/About';
+// import AddNewFile from './MyComponents/AddNewFile';
 function App() {
   return (
-    <div className="App">
-      
-          Learn React
-       
-    </div>
+    <>
+      {/* <Router> */}
+        <Header title="Json Grid Viewer"/>
+        <About />
+        <AddURL/>
+        <AddJSON />
+        {/* <AddNewFile /> */}
+    </>
   );
 }
 
