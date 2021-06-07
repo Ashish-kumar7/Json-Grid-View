51d6bb99d891db7480214e83d71c1aa88a44e776
ashish007kumar <ashishkumar357ak@gmail.com>
2021-06-07 21:14:51 +0530

    Added unit test case for Json

--- backend/test_api.py
+++ backend/test_api.py
@@ -7,7 +7,20 @@ class ApiTest(unittest.TestCase):
     STR_URL_INPTYPE = "url"
     STR_URL = "https://api.github.com/users/hadley/orgs"
 
-    def test_string(self):
+    STR_STRING_INPTYPE = "text"
+    STR_JSON = \
+        """
+        {
+            "name" : "Ash",
+            "id" : "123"
+        }
+        """
+
+    def test_string_URL(self):
         r = requests.post(ApiTest.API_URL, data = dict(input_type=ApiTest.STR_URL_INPTYPE, Url=ApiTest.STR_URL))
         self.assertEqual(r.status_code, 200)
         
+
+    def test_string_JSON(self):
+        r = requests.post(ApiTest.API_URL, data = dict(input_type=ApiTest.STR_STRING_INPTYPE, Json=ApiTest.STR_JSON))
+        self.assertEqual(r.status_code, 200)
