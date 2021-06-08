7d17e2d98d555599f79c5cd43fa3e7165dd8745b
unknown <notabhishektiwari@gmail.com>
2021-06-08 18:20:47 +0530

    add code to handle missing values, with and without adding index

--- backend/App.py
+++ backend/App.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify, request, send_file
 from flask_cors import CORS, cross_origin
 import json
 import urllib.request
-import utilities 
+import utilities
 import pandas as pd
 # import numpy as np
 import sqlalchemy
@@ -15,13 +15,20 @@ app = Flask(__name__)
 CORS(app)
 
 # Constants
+
+# File Constants
 CSV_FILENAME = 'generatedCsvFile'
 XLSX_FILENAME = 'generatedXlsxFile'
 SQL_DB_NAME = 'generatedDB'
 SQL_TAB_NAME = 'table001'
 
-
-
+# Generation constants
+JOINER_CHAR = '.'
+JOIN_PAR_IN_COLS = True
+REPEAT_IN_COL = True
+ADD_INDEX_FOR_LIST = False
+INDEX_FOR_LIST_SUFFIX = 'INDEX'  # Index colname = par + joiner + index_suffix
+FILL_MISSING_WITH = 'null'
 
 
 @app.route('/api/upload', methods=['POST'])
@@ -31,7 +38,7 @@ def uploadFile():
     jsonData = {}
 
     try:
-        print("form  : " , request.form)
+        print("form  : ", request.form)
         startTime = time.time()
         initTime = startTime
 
@@ -52,24 +59,39 @@ def uploadFile():
         # print(jsonData)
 
         startTime = time.time()
-        columnList, tableSchema = utilities.GenTableSchema(jsonData)
+        columnList, tableSchema, columnListOrd, tableSchemaOrd = utilities.GenTableSchema(
+            jsonData, JOINER_CHAR=JOINER_CHAR,  ADD_INDEX_FOR_LIST=ADD_INDEX_FOR_LIST,
+                            INDEX_FOR_LIST_SUFFIX=INDEX_FOR_LIST_SUFFIX)
         print("Time to gen schema : ", time.time() - startTime)
         # Generated columnList and schemaTree
-        # print("columns : " , columnList)
+        # print("columns : ", columnList)
         # print("schema Tree: ", tableSchema)
+        # print("ord cols :" , columnListOrd)
+        # print("schema Tree Ord: ", tableSchemaOrd)
+
 
         # startTime = time.time()
         # DF = pd.DataFrame(columns=columnList)
         # print("Time to create empty df : ", time.time() - startTime)
 
+        # print("Joiner : ", JOINER_CHAR)
+        # print("Repeat same value in col : ", REPEAT_IN_COL)
+        # print("Add index for list : ", ADD_INDEX_FOR_LIST,
+        #       " suffix :", INDEX_FOR_LIST_SUFFIX)
+        # print("Join Par in Cols : ",  JOIN_PAR_IN_COLS)
+        # print("Fill Missing with : ", FILL_MISSING_WITH)
+
         DataDict = {}
         startTime = time.time()
-        utilities.WriteDict(DataDict, 0, '', jsonData)
+        utilities.WriteData(DataDict, jsonData, tableSchema, FILL_MISSING_WITH=FILL_MISSING_WITH, ADD_INDEX_FOR_LIST=ADD_INDEX_FOR_LIST,
+                            INDEX_FOR_LIST_SUFFIX=INDEX_FOR_LIST_SUFFIX)
+        # print(DataDict)
         print("Time to create DataDict: ", time.time() - startTime)
 
         startTime = time.time()
-        DF = pd.DataFrame.from_dict(DataDict, "index")
-        print("Time to create DF from Dict: ", time.time() - startTime)
+        columnsOrder = columnListOrd
+        DF = pd.DataFrame(list(DataDict.values()), columns=columnsOrder)
+        print("Time to create DF from Dict in order: ", time.time() - startTime)
 
         # startTime = time.time()
         # utilities.WriteToDF(DF, jsonData, tableSchema)
@@ -89,14 +111,14 @@ def uploadFile():
             startTime = time.time()
             DF.to_csv(CSV_FILENAME + '.csv')
             print("Time to gen csv : ", time.time() - startTime)
-            return send_file(filename_or_fp =CSV_FILENAME + '.csv' )
+            return send_file(CSV_FILENAME + '.csv')
 
         # Generate XLSX
         if extension == "excel":
             startTime = time.time()
             DF.to_excel(XLSX_FILENAME + '.xlsx')
             print("Time to gen xlsx : ", time.time() - startTime)
-            return send_file(filename_or_fp =XLSX_FILENAME + '.xlsx',as_attachment=True, mimetype="EXCELMIME")
+            return send_file(XLSX_FILENAME + '.xlsx', as_attachment=True, mimetype="EXCELMIME")
 
         # Generate SQL Database, Table
         if extension == "hive":
@@ -109,21 +131,20 @@ def uploadFile():
             # print(engine.execute("SELECT * FROM " + tableName).fetchall())
             sqlite_connection.close()
             print("Time to gen db : ", time.time() - startTime)
-            
+
             startTime = time.time()
             print("Total time taken : ", startTime - initTime)
-            
-            # code to convert csv file and saving it to hdfs
-            df = pd.read_csv('generatedCsvFile.csv')
-            df.to_parquet("/test_parquet", compression="GZIP")
-            hdfs_cmd = "hadoop fs -put /test_parquet /hbase/outputFileP"
-            subprocess.call(hdfs_cmd, shell=True)
 
-            return send_file(filename_or_fp =SQL_DB_NAME + '.db')
-        
+            # code to convert csv file and saving it to hdfs
+            # df = pd.read_csv('generatedCsvFile.csv')
+            # df.to_parquet("/test_parquet", compression="GZIP")
+            # hdfs_cmd = "hadoop fs -put /test_parquet /hbase/outputFileP"
+            # subprocess.call(hdfs_cmd, shell=True)
+
+            return send_file(SQL_DB_NAME + '.db')
+
         # response = jsonify(message="Api server is running")
         # return response
-
 
     except Exception as e:
         print(e)
