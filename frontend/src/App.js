81040e795284a3323a8b808855db07cd7669412a
ashish007kumar <ashishkumar357ak@gmail.com>
2021-06-10 23:46:17 +0530

    added the footer2 component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import HomePage from './pages/homepage/HomePage';
 import OptionsPage from './pages/optionpage/OptionsPage';
 import FileUpload from './pages/fileupload/FileUpload';
 import Footer from './components/footer/Footer'
+import Footer2 from './components/footer2/Footer2'
 import FileUrl from './pages/fileurl/FileUrl'
 import JsonInput from './pages/jsoninput/JsonInput'
 import Editor from './components/editor/Editor'
@@ -11,8 +12,9 @@ import Editor from './components/editor/Editor'
 function App() {
   return (
     <div>
-        <Route path='/editor'><Editor></Editor></Route>
-        <Route path='/footer'><Footer></Footer></Route>
+        <Route path='/editor'><Editor /></Route>
+        <Route path='/footer'><Footer /></Route>
+        <Route path='/footer2'><Footer2 /></Route>
         <Route  path="/home" ><HomePage /></Route>
         <Route path="/options"><OptionsPage/></Route>
         <Route path="/file-upload"><FileUpload /></Route>
