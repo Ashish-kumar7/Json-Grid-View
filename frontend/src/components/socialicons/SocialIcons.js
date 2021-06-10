9edb7cec7b0cb6ed3e92c957279382ac8140980e
ashish007kumar <ashishkumar357ak@gmail.com>
2021-06-10 22:46:28 +0530

    added the different icons

--- frontend/src/components/socialicons/SocialIcons.js
+++ frontend/src/components/socialicons/SocialIcons.js
@@ -1,25 +1,35 @@
 import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faGithub, faInstagram, faFacebookF} from '@fortawesome/free-brands-svg-icons'
+import {faGithub, faInstagram, faFacebookF, faLinkedinIn,faAppStore } from '@fortawesome/free-brands-svg-icons'
 import "./SocialIcons.css";
 
 const SocialIcons = () => {
   return (
     <Fragment>
       <span className="d-inline">
-        <Link to="/" className="iconf d-inline">
+        <Link to="/home" className="iconf d-inline">
           <FontAwesomeIcon icon={faFacebookF} size="lg" />
         </Link>
       </span>
       <span className="d-inline">
-        <Link to="/" className="iconf">
+        <Link to="/home" className="iconf">
+          <FontAwesomeIcon icon={faInstagram} size="lg" />
+        </Link>
+      </span>
+      <span className="d-inline">
+        <Link to="/home" className="iconf">
           <FontAwesomeIcon icon={faGithub} size="lg" />
         </Link>
       </span>
       <span className="d-inline">
-        <Link to="/" className="iconf">
-          <FontAwesomeIcon icon={faInstagram} size="lg" />
+        <Link to="/home" className="iconf">
+          <FontAwesomeIcon icon={faLinkedinIn} size="lg" />
+        </Link>
+      </span>
+      <span className="d-inline">
+        <Link to="/home" className="iconf">
+          <FontAwesomeIcon icon={faAppStore} size="lg" />
         </Link>
       </span>
     </Fragment>
