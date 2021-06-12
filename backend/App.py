7c002f925efb548d8daa54e59927eafb4efd7f32
unknown <notabhishektiwari@gmail.com>
2021-06-12 18:44:39 +0530

    upd joiner char code

--- backend/App.py
+++ backend/App.py
@@ -1,10 +1,16 @@
+HADOOP_INSTALLED = False
+
+
 from logging import exception
 from flask import Flask, jsonify, request, send_file
 from flask_cors import CORS, cross_origin
 import json
 import urllib.request
 import utilities
-import hadoopstorage
+
+if HADOOP_INSTALLED:
+    import hadoopstorage
+
 import pandas as pd
 # import numpy as np
 import sqlalchemy
@@ -23,7 +29,7 @@ CSV_FILENAME = 'generatedCsvFile'
 XLSX_FILENAME = 'generatedXlsxFile'
 SQL_DB_NAME = 'generatedDB'
 SQL_TAB_NAME = 'table001'
-
+SHEET_NAME = 'Sheet1'
 
 # Generation constants
 JOINER_CHAR = '.'
@@ -33,6 +39,13 @@ ADD_INDEX_FOR_LIST = False
 INDEX_FOR_LIST_SUFFIX = 'INDEX'  # Index colname = par + joiner + index_suffix
 FILL_MISSING_WITH = 'null'
 GEN_CROSS_TABLE = False
+TABLE_TYPE = '1'
+
+ROWS_PER_PAGE = 50
+CURRENT_PAGE = 1
+TOTAL_PAGES = 1
+
+
 
 DF = ''
 jsonData ={}
@@ -49,9 +62,9 @@ def connected():
 @app.route('/api/upload', methods=['POST'])
 @cross_origin()
 def uploadFile():
-
     global jsonData 
-    
+
+    print("\n\n\n\nForm Data in /api/upload\n" , request.form)
     try:
         print("form  : ", request.form)
         startTime = time.time()
@@ -78,19 +91,7 @@ def uploadFile():
         # Json Loaded Successfully!
         # print(jsonData)
 
-        startTime = time.time()
         
-        global tableSchema, columnListOrd
-        columnList, tableSchema, columnListOrd, tableSchemaOrd = utilities.GenTableSchema(
-            jsonData, JOINER_CHAR=JOINER_CHAR,  ADD_INDEX_FOR_LIST=ADD_INDEX_FOR_LIST,
-                            INDEX_FOR_LIST_SUFFIX=INDEX_FOR_LIST_SUFFIX)
-        print("Time to gen schema : ", time.time() - startTime)
-        
-        # Generated columnList and schemaTree
-        # print("columns : ", columnList)
-        # print("schema Tree: ", tableSchema)
-        # print("ord cols :" , columnListOrd)
-        # print("schema Tree Ord: ", tableSchemaOrd)
 
 
         # startTime = time.time()
@@ -192,7 +193,49 @@ def uploadFile():
 @cross_origin()
 def processFile():
     print("process")
+    print("\n\n\n\nForm Data in /api/process\n" , request.form)
     try:
+        # Assign global variables based on received
+        global JOINER_CHAR
+        global JOIN_PAR_IN_COLS
+        global SHEET_NAME
+        global SQL_TAB_NAME
+        global TABLE_TYPE
+        global FILL_MISSING_WITH
+
+        JOINER_CHAR = request.form['join_char']
+        JOIN_PAR_IN_COLS = True if request.form['parentCol']=='true' else False
+        SHEET_NAME = request.form['sheetName']
+        SQL_TAB_NAME = request.form['tableName']
+        FILL_MISSING_WITH = request.form['nullName']
+        TABLE_TYPE = request.form['table_type']
+
+        # Fill remaining based on TABLE_TYPE
+        if TABLE_TYPE == '1' :
+            ADD_INDEX_FOR_LIST = False
+            GEN_CROSS_TABLE = False
+        elif TABLE_TYPE == '2' :
+            ADD_INDEX_FOR_LIST = False
+            GEN_CROSS_TABLE = True
+        elif TABLE_TYPE == '3' :
+            ADD_INDEX_FOR_LIST = True
+            GEN_CROSS_TABLE = False
+
+        startTime = time.time()
+        
+        global tableSchema, columnListOrd
+        columnList, tableSchema, columnListOrd, tableSchemaOrd = utilities.GenTableSchema(
+            jsonData, JOINER_CHAR=JOINER_CHAR,  ADD_INDEX_FOR_LIST=ADD_INDEX_FOR_LIST,
+                            INDEX_FOR_LIST_SUFFIX=INDEX_FOR_LIST_SUFFIX)
+        print("Time to gen schema : ", time.time() - startTime)
+        
+        # Generated columnList and schemaTree
+        # print("columns : ", columnList)
+        # print("schema Tree: ", tableSchema)
+        # print("ord cols :" , columnListOrd)
+        # print("schema Tree Ord: ", tableSchemaOrd)
+
+
         DataDict = {}
         startTime = time.time()
         socketio.emit('progress', 50, broadcast=True)
@@ -212,9 +255,12 @@ def processFile():
         DF = pd.DataFrame(list(DataDict.values()), columns=columnsOrder)
         print("Time to create DF from Dict in order: ", time.time() - startTime)
 
-        response = jsonify(message="Data frame generated")
+        html_string = DF.to_html(classes='mystyle')
+
+        response = jsonify(table=html_string) 
         return response
-        
+    
+    
     except Exception as e:
         print(e)
         return jsonify({'message:', 'error'})
@@ -225,6 +271,7 @@ def processFile():
 @cross_origin()
 def convertFile():
     print("convert")
+    print("\n\n\n\nForm Data in /api/convert\n" , request.form)
     try:
         extension = request.form['content_type']
         print(extension)
@@ -248,9 +295,6 @@ def convertFile():
         if extension == "hive":
             
             startTime = time.time()
-            print(type(DF))
-            DF.to_csv( 'test.csv')
-            hadoopstorage.saveFile(DF)
             sql_engine = sqlalchemy.create_engine(
                 'sqlite:///' + SQL_DB_NAME + '.db', echo=False)
             sqlite_connection = sql_engine.connect()
@@ -263,17 +307,20 @@ def convertFile():
             startTime = time.time()
             print("Total time taken : ", startTime - initTime)
             socketio.emit('progress', 80, broadcast=True)
-            # code to convert csv file and saving it to hdfs
-            # df = pd.read_csv('generatedCsvFile.csv')
-            # df.to_parquet("/test_parquet", compression="GZIP")
-            # hdfs_cmd = "hadoop fs -put /test_parquet /hbase/storedCSV"
-            # subprocess.call(hdfs_cmd, shell=True)
+            if HADOOP_INSTALLED :
+                DF.to_csv( 'test.csv')
+                hadoopstorage.saveFile(DF)
+                # code to convert csv file and saving it to hdfs
+                df = pd.read_csv('generatedCsvFile.csv')
+                df.to_parquet("/test_parquet", compression="GZIP")
+                hdfs_cmd = "hadoop fs -put /test_parquet /hbase/storedCSV"
+                subprocess.call(hdfs_cmd, shell=True)
 
-            # code to convert csv file and saving it to hdfs
-            # df = pd.read_csv('generatedCsvFile.csv')
-            # df.to_parquet("/test_parquet", compression="GZIP")
-            # hdfs_cmd = "hadoop fs -put /test_parquet /hbase/outputFileP"
-            # subprocess.call(hdfs_cmd, shell=True)
+                # code to convert csv file and saving it to hdfs
+                df = pd.read_csv('generatedCsvFile.csv')
+                df.to_parquet("/test_parquet", compression="GZIP")
+                hdfs_cmd = "hadoop fs -put /test_parquet /hbase/outputFileP"
+                subprocess.call(hdfs_cmd, shell=True)
 
             return send_file(SQL_DB_NAME + '.db')
 
