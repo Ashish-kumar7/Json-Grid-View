10e59bb4df4474e1be285a41c5feb13462f24fe4
ashish007kumar <ashishkumar357ak@gmail.com>
2021-06-12 10:36:07 +0530

    Added Jest unit test for prop types

--- frontend/src/components/iconbox/iconbox.test.js
+++ frontend/src/components/iconbox/iconbox.test.js
@@ -2,10 +2,28 @@ import React from 'react';
 import IconBox from './iconbox';
 import Adapter from 'enzyme-adapter-react-16';
 import { shallow, configure, ShallowWrapper } from 'enzyme';
+import PropTypes from 'prop-types';
 
 configure({ adapter: new Adapter() });
 
 let shallowWrapper = ShallowWrapper;
+
+const consoleErrorSpy = jest.spyOn(global.console, 'error');
+
+jest.mock('prop-types', () => {
+    const RealPropTypes = jest.requireActual('prop-types');
+    const mockPropTypes = jest.requireActual('mock-prop-types');
+
+    return mockPropTypes(RealPropTypes);
+});
+
+describe('IconBoxComponent PropType', () => {
+    it('exposes the expected propTypes', () => {
+        expect(IconBox.propTypes).toEqual({
+            size: PropTypes.string
+        });
+    });
+});
 
 beforeEach(() => {
     shallowWrapper = shallow(<IconBox />);
@@ -45,3 +63,12 @@ it('able to find the count of <FontAwesomeIcon> element', () => {
     expect(shallowWrapper.find('FontAwesomeIcon').length).toBe(1);
 });
 
+//Props recieved into the button 
+it('throws error if given the wrong props', () => {
+    shallow(
+        <IconBox
+            size={123}
+        />
+    );
+    expect(consoleErrorSpy).toBeCalled();
+});
