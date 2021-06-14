ad4263d3da3dbc4107f65030d59c2b346a65adf6
ashish007kumar <ashishkumar357ak@gmail.com>
2021-06-14 21:25:22 +0530

    Added comments

--- tech-explored/backend-java/src/main/java/com/jsonparser/jsonparser/exceltosql/JsonParser.java
+++ tech-explored/backend-java/src/main/java/com/jsonparser/jsonparser/exceltosql/JsonParser.java
@@ -3,8 +3,6 @@ package com.jsonparser.jsonparser.exceltosql;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-//import com.jsonparser.jsonparser.util.JFlat;
-
 import com.github.opendevl.JFlat;
 import com.jsonparser.jsonparser.util.CsvToExcel;
 
@@ -13,9 +11,10 @@ public class JsonParser {
 	public void parseJson(String jsonFile) throws Exception {
 
 		System.gc();
+		//Start Time
 		long startTime = System.currentTimeMillis();
 		System.out.println("Start Time:" + String.valueOf(startTime));
-
+		//Starting to read the file
 		System.out.println("File read starting!");
 		String str = new String(Files.readAllBytes(Paths.get(jsonFile)));
 		System.out.println("File read completed!");
@@ -24,12 +23,12 @@ public class JsonParser {
 
 		// get the 2D representation of JSON document
 		flatMe.json2Sheet().headerSeparator("_").getJsonAsSheet();
-
+	
 		System.out.println("Writing now!");
 		// write the 2D representation in csv format
 		String csvFileName = "E:\\DBInternProject\\testing\\test1.csv";
 		flatMe.write2csv(csvFileName);
-
+		//End Time
 		long endTime = System.currentTimeMillis();
 		System.out.println("Time Taken:" + String.valueOf(endTime - startTime));
 
