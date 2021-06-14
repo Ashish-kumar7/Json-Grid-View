1e010dee6c40b544dfc6cd072addfe0d9b87fc9d
ashish007kumar <ashishkumar357ak@gmail.com>
2021-06-14 21:48:23 +0530

    Added Commnets

--- backend/hadoopstorage.py
+++ backend/hadoopstorage.py
@@ -1,7 +1,14 @@
 import os
 import pandas as pd
-os.environ["JAVA_HOME"] = "C:/Progra~1/Java/jdk1.8.0_65"
-os.environ["SPARK_HOME"] = "E:/hadoop-env/spark-3.0.2-bin-hadoop2.7"
+
+# Add the appropriate locations of the Hadoop and Java.
+
+# os.environ["JAVA_HOME"] = "C:\Progra~1\Java\jdk-12.0.1"
+# os.environ["SPARK_HOME"] = "C:\Spark"
+
+# os.environ["JAVA_HOME"] = "C:/Progra~1/Java/jdk1.8.0_65"
+# os.environ["SPARK_HOME"] = "E:/hadoop-env/spark-3.0.2-bin-hadoop2.7"
+
 import findspark
 findspark.init()
 
@@ -26,6 +33,11 @@ def saveFile(DF):
                 .option('header',True) \
                 .option('multiLine', True) \
                 .load('test.csv')
-    df.write.format("parquet").mode("overwrite").save("hdfs://0.0.0.0:19000/testFile.parquet")
-    df_load = spark.read.load('hdfs://0.0.0.0:19000/testFile.parquet')
+
+    # add the appropriate localhost URL
+
+    # df.write.format("parquet").mode("overwrite").save("hdfs://localhost:9000/testFile.parquet")
+    # df_load = spark.read.load('hdfs://localhost:9000/testFile.parquet')
+    # df.write.format("parquet").mode("overwrite").save("hdfs://0.0.0.0:19000/testFile.parquet")
+    # df_load = spark.read.load('hdfs://0.0.0.0:19000/testFile.parquet')
     df_load.show()
