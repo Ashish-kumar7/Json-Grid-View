b2734d569e26a10f1414407c5092321e0993fea7
unknown <notabhishektiwari@gmail.com>
2021-06-16 15:43:15 +0530

    upd Preivew.js

--- frontend/src/pages/previewpage/PreviewPage.js
+++ frontend/src/pages/previewpage/PreviewPage.js
@@ -70,14 +70,8 @@ const PreviewPage = (props) => {
   const [formDisplay, setFormDisplay] = useState(false);
   const [uniqueRowsPerPage, setUniqueRowsPerPage] = useState(1);
   const [uniqueTotalRecords, setUniqueTotalRecords] = useState(1);
-<<<<<<< HEAD
-  const [resultTotalRecords, setResultTotalRecords] = useState(initialDataFrame.records);
-  const [resultRows, setResultRows] = useState(initialDataFrame.rows);
-  
-=======
   const [resultTotalRecords, setResultTotalRecords]= useState(initialDataFrame.records);
   const [resultRows, setResultRows]= useState(initialDataFrame.rows);
->>>>>>> b4b90d7b65e14b735dd488493ecb33623965ca65
   const [showValue, setShowValue] = useState(false);
   // unique 20 values of a particular column
   let [values, setValues] = useState([]);
@@ -431,15 +425,9 @@ const PreviewPage = (props) => {
         </div>
         <div className={classes.num}>
           <PaginationP
-<<<<<<< HEAD
             key={ resultTotalRecords }
             totalRecords={ resultTotalRecords }
             pageLimit={ resultRows }
-=======
-            key={resultTotalRecords}
-            totalRecords={resultTotalRecords}
-            pageLimit={resultRows}
->>>>>>> b4b90d7b65e14b735dd488493ecb33623965ca65
             pageNeighbours={1}
             onPageChanged={onPageChanged}
           />
