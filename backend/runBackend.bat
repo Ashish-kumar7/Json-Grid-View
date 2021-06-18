649795bf847b6f5d75134c0650c44f94e5e5e79e
unknown <notabhishektiwari@gmail.com>
2021-06-18 14:07:21 +0530

    Add .bat scripts to start backend and frontend automatically

---
+++ backend/runBackend.bat
@@ -0,0 +1,10 @@
+@echo off
+title Starting Json-Grid-View/backend
+color 1b
+
+echo Starting backend ...
+echo #################################
+echo The current directory is %CD%
+
+python "App.py"
+pause
