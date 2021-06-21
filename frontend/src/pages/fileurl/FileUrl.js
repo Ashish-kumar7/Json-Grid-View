e2e0d853685fdbd5a9a42eccf6c0b063deedcd23
ashish007kumar <ashishkumar357ak@gmail.com>
2021-06-21 17:27:59 +0530

    Refactored the code

--- frontend/src/pages/fileurl/FileUrl.js
+++ frontend/src/pages/fileurl/FileUrl.js
@@ -1,4 +1,3 @@
-
 import Navbar from "../../components/navbar/Navbar";
 import "./FileUrl.css";
 import { useState } from "react";
@@ -9,8 +8,6 @@ import Modal from "../../components/modal/Modal";
 import { css } from "@emotion/react";
 import RingLoader from "react-spinners/RingLoader";
 
-
-// const socket = io("http://localhost:5000/");
 const override = css`
   display: block;
   margin: 0 auto;
@@ -21,13 +18,11 @@ const FileUrl = () => {
   const [inputUrl, setInputUrl] = useState();
   // const [uploadPercentage, setUploadPercentage] = useState(0);
 
-
-
   let [loading, setLoading] = useState(false);
   let [color, setColor] = useState("#ea80fc");
 
-  const [disable ,setDisable ]= useState(false);
-  const [buttonId,setButtonId] = useState("downloadButton");
+  const [disable, setDisable] = useState(false);
+  const [buttonId, setButtonId] = useState("downloadButton");
 
   // on clicking customize button modal will be shown (show modal variable)
   const [open, setOpen] = useState(false);
@@ -52,124 +47,46 @@ const FileUrl = () => {
     setOpen(true);
   };
 
-  // const handleSubmission = () => {
-  //   const formData = new FormData();
-  //   formData.append("Url", inputUrl);
-  //   formData.set("input_type", "url");
-  //   console.log(inputUrl);
-  //   console.log(formData);
-  // const options = {
-  //   onUploadProgress: (progressEvent) => {
-  //     const { loaded, total } = progressEvent;
-  //     let percent = Math.floor((loaded * 100) / total);
-  //     console.log(`${loaded}kb of ${total}kb | ${percent}%`);
-
-  //     if (percent < 100) {
-  //       setUploadPercentage(percent);
-  //       console.log(uploadPercentage);
-  //     }
-  //   },
-  // };
-  // fetch("http://localhost:5000/api/upload", {
-  //   headers: {
-  //     "Access-Control-Allow-Origin": "*",
-  //   },
-  //   method: "POST",
-  //   body: formData,
-  // })
-  //   .then((response) => response.json())
-  //   .then((result) => {
-  //     console.log("Success", result);
-  //   })
-  //   .catch((error) => {
-  //     console.error("Error", error);
-  //   });
-  //   axios
-  //     .post("http://localhost:5000/api/upload", formData)
-  //     .then((res) => {
-  //       console.log("data frame generated");
-  //       setProcessed(false);
-  //       setShowOptions(true);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //       validJSON = false;
-  //       alert("Invalid JSON URL !!");
-  //     });
-  // };
-
-  // const handleConversion = (val) => {
-  //   const formData = new FormData();
-  //   formData.set("content_type", val);
-  //   if (val == "excel") {
-  //     setFileExtension("output.xlsx");
-  //   } else if (val == "csv") {
-  //     setFileExtension("output.csv");
-  //   } else {
-  //     setFileExtension("output.db");
-  //   }
-  //   axios
-  //     .post(
-  //       "http://localhost:5000/api/convert",
-  //       formData,
-  //       { responseType: "blob" }
-  //     )
-  //     .then((response) => {
-  //       setUploadPercentage(100);
-  //       setTimeout(() => {
-  //         setUploadPercentage(0);
-  //       }, 1000);
-  //       setDownloadContent(response.data);
-  //       console.log(response);
-  //       setShowDownload(true);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //       setUploadPercentage(0);
-  //       alert("Oops it Breaks!!" + err);
-  //     });
-  // }
-  
   const handleCustomize = () => {
-    if(disable){
+    if (disable) {
       console.log("disable true");
     }
-    else{
-    setDisable(true);
-    setButtonId("disableButton");
-    setLoading(true);
+    else {
+      setDisable(true);
+      setButtonId("disableButton");
+      setLoading(true);
 
-    const formData = new FormData();
-    formData.append("Url", inputUrl);
-    formData.set("input_type", "url");
-    axios
-      .post("http://localhost:5000/api/upload", formData)
-      .then((res) => {
-        console.log("json loaded and checked");
-        setLoading(false);
+      const formData = new FormData();
+      formData.append("Url", inputUrl);
+      formData.set("input_type", "url");
+      axios
+        .post("http://localhost:5000/api/upload", formData)
+        .then((res) => {
+          console.log("json loaded and checked");
+          setLoading(false);
 
-        if (res.data.message.startsWith("Error")) {
+          if (res.data.message.startsWith("Error")) {
+            setDisable(false);
+            setButtonId("downloadButton");
+            alert(res.data.message);
+          } else {
+            setDisable(false);
+            setButtonId("downloadButton");
+            showModal();
+          }
+        })
+
+        .catch((err) => {
+          // display alert for wrong json
+          setLoading(false);
           setDisable(false);
           setButtonId("downloadButton");
-          alert(res.data.message);
-        } else {
-          setDisable(false);
-          setButtonId("downloadButton");
-          showModal();
-        }
-      })
-
-      .catch((err) => {
-        // display alert for wrong json
-        setLoading(false);
-        setDisable(false);
-        setButtonId("downloadButton");
-        console.log(err);
-        validJSON = false;
-        setTimeout(() => {
-          alert("Invalid JSON URL !!");
-        }, 1000);
-      });
+          console.log(err);
+          validJSON = false;
+          setTimeout(() => {
+            alert("Invalid JSON URL !!");
+          }, 1000);
+        });
     }
   };
 
@@ -183,7 +100,7 @@ const FileUrl = () => {
       <RingLoader color={color} loading={loading} css={override} size={150} />
 
       <>
-       <Button
+        <Button
           title={"Customize"}
           classId={buttonId}
           clickFunc={() => handleCustomize()}
@@ -195,18 +112,6 @@ const FileUrl = () => {
         ></Modal>
       </>
       )
-
-      {/* {uploadPercentage > 0 && (
-        <div className="progressbar">
-          <ProgressBar
-            now={uploadPercentage}
-            striped={true}
-            animated
-            label={`${uploadPercentage}%`}
-            variant="success"
-          />
-        </div> 
-        )} */}
 
     </div>
   );
