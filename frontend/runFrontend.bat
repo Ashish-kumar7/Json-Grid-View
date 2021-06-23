e5fd46e75b2e9833bcd56ab68e79746b53b1a4d5
ashish007kumar <ashishkumar357ak@gmail.com>
2021-06-23 23:10:44 +0530

    .bat file for Frontend

--- frontend/runFrontend.bat
+++ frontend/runFrontend.bat
@@ -1,13 +1,15 @@
 @echo off
 title Starting Json-Grid-View/frontend
-color 1b
 
 echo Starting frontend ...
-echo #################################
 echo The current directory is %CD%
 
 echo performing -- npm install
 call npm install
+
+echo performing -- npm run
+call npm run
+
 echo performing -- npm start
 call npm start 
 
