a4c568b0ec3636b7a541cfaad2f39fd64a18c03a
ashish007kumar <ashishkumar357ak@gmail.com>
2021-06-23 10:21:54 +0530

    Removed the Query Button

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
                         <Link to="/json-input"><button id="b1" class="btn btn-lg btn-info" >Editor</button></Link>
                         <Link to="/file-upload"><button id="b2" class="btn btn-lg " >Upload</button></Link>
                         <Link to="/file-url"><button id="b3" class="btn btn-lg " >URL</button></Link>
-                        <button id="b4" class="btn btn-lg " >Query</button>
+                        {/* <button id="b4" class="btn btn-lg " >Query</button> */}
                         <Link to="/jsonchecker"><button id="b5" class="btn btn-lg ">JSON Checker</button></Link>
                         {/* <Link to="/query-page"><button id="b6" class="btn btn-lg btn-primary">SQL _Queries</button></Link> */}
                     </form>
