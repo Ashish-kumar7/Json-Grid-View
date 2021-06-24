fe1b7c50e00a952166c32fa85b8435acb2cae9d1
ashish007kumar <ashishkumar357ak@gmail.com>
2021-06-24 09:46:29 +0530

    updated the .bat file

--- Start Json-Grid-View.bat
+++ Start Json-Grid-View.bat
@@ -10,14 +10,22 @@ echo Starting frontend server
 start cmd.exe /k "call runFrontend.bat"
 echo #################################
 
+@REM cd ..
+
+@REM cd backend
+@REM echo Starting backend server for virtual env
+@REM start cmd.exe /k "call runBackend2.bat"
+@REM echo #################################
+
 cd ..
 
 cd backend
-echo Starting backend server
-start cmd.exe /k "call runBackend2.bat"
+echo Starting backend server globally
+start cmd.exe /k "call runBackend.bat"
 echo #################################
 
 cd ..
+
 cd backend
 echo starting hadoop
 start cmd.exe /k "call starthadoop.bat"
