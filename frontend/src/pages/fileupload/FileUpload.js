3d8d0bb1db42faee5ed030a1934c81e1049e72b0
Aditi Khandelwal <aditikhandelwal2000@gmail.com>
2021-06-27 19:55:53 +0530

    Added comments and responsiveness for file upload page

--- frontend/src/pages/fileupload/FileUpload.js
+++ frontend/src/pages/fileupload/FileUpload.js
@@ -2,128 +2,103 @@ import "./FileUpload.css";
 import { faUpload } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
-import { ProgressBar } from "react-bootstrap";
 import Navbar from "../../components/navbar/Navbar";
 import axios from "axios";
 import Button from "../../components/button/Button";
-import io from "socket.io-client";
 import Modal from "../../components/modal/Modal";
 import { css } from "@emotion/react";
 import RingLoader from "react-spinners/RingLoader";
-import BounceLoader from "react-spinners/BounceLoader";
-import ClockLoader from "react-spinners/ClockLoader";
 
-
-
-// const socket = io("http://localhost:5000/");
-
+// css for loader
 const override = css`
   display: block;
   margin: 0 auto;
   border-color: red;
 `;
 
-
 const FileUpload = () => {
+  // store the selected json file , when uploaded from pc or laptop
   const [selectedFile, setSelectedFile] = useState();
-  // to check if file is selected or not
+  // check if file is selected or not
   const [isSelected, setIsSelected] = useState(false);
+  // used to show or hide the customize button
   const [customize, setCustomize] = useState(false);
   const [showCustomizeButton, setShowCustomizeButton] = useState(true);
-
+  // used to show or hide the loader
   let [loading, setLoading] = useState(false);
+  // stores color of loader
   let [color, setColor] = useState("#ea80fc");
-
-  const [disable ,setDisable ]= useState(false);
-  const [buttonId,setButtonId] = useState("downloadButton");
-
-  // on clicking customize button modal will be shown (show modal variable)
+  // once a button is clicked  it is used to set disable property for button
+  const [disable, setDisable] = useState(false);
+  // to change the button id to disableButton when disable variable is set true
+  const [buttonId, setButtonId] = useState("downloadButton");
+  // on clicking customize button modal will be shown (show customization page)
   const [open, setOpen] = useState(false);
-
+  // to check json is valid or not
   var validJSON = true;
 
+  // function to close customization page
   const hideModal = () => {
     setOpen(false);
   };
 
+  // function to open customization page
   const showModal = () => {
     setOpen(true);
   };
 
-  // for getting updates regarding progress
-  // socket.on("progress", (val) => {
-  //   setUploadPercentage(val);
-  //   console.log(val);
-  // });
-
-  // on selecting file for upload this function is called
+  // this function is called on selecting file for upload
   const changeHandler = (e) => {
     if (e.target.files[0]) {
+      // here file is stored and customization button will be shown
       setSelectedFile(e.target.files[0]);
       setIsSelected(true);
       setCustomize(true);
     }
   };
 
-  // on clicking customize button ,file will be sent to backend, schema will be received and customize modal will be shown
+  // on clicking customize button this function is called ,json file will be sent to backend, on receiving response with response status 200 - customization page will be shown
   const handleCustomize = () => {
-    if(disable){
+    if (disable) {
       console.log("disable true");
-    }
-    else{
-    setDisable(true);
-    setButtonId("disableButton");
-    setLoading(true);
-    const formData = new FormData();
-    formData.append("File", selectedFile);
-    formData.set("input_type", "file");
+    } else {
+      setDisable(true);
+      setButtonId("disableButton");
+      setLoading(true);
+      // sending data - file and type of data - file to backend
+      const formData = new FormData();
+      formData.append("File", selectedFile);
+      formData.set("input_type", "file");
 
-    axios
-      .post("http://localhost:5000/api/upload", formData)
-      .then((res) => {
-        console.log("json loaded and checked");
-        setLoading(false);
-        if (res.data.message.startsWith("Error")) {
+      // this api checks whether json in file is valid or not
+      axios
+        .post("http://localhost:5000/api/upload", formData)
+        .then((res) => {
+          console.log("json loaded and checked");
+          setLoading(false);
+          if (res.data.message.startsWith("Error")) {
+            setDisable(false);
+            setButtonId("downloadButton");
+            alert(res.data.message);
+          } else {
+            setDisable(false);
+            setButtonId("downloadButton");
+            showModal();
+          }
+        })
+        .catch((err) => {
+          // display alert for wrong json
+          setLoading(false);
           setDisable(false);
           setButtonId("downloadButton");
-          alert(res.data.message);
-        }
-        else {
-          setDisable(false);
-          setButtonId("downloadButton");
-          showModal();
-        }
-      })
-      .catch((err) => {
-        // display alert for wrong json
-        setLoading(false);
-        setDisable(false);
-        setButtonId("downloadButton");
-        console.log(err);
-        validJSON = false;
-        setTimeout(() => {
-          alert("Invalid JSON File !!");
-        }, 1000);
-      });
+          console.log(err);
+          validJSON = false;
+          setTimeout(() => {
+            alert("Invalid JSON File !!");
+          }, 1000);
+        });
     }
   };
-  //   axios
-  //     .post("http://localhost:5000/api/convert", formData, {
-  //       responseType: "blob",
-  //     })
-  //     .then((response) => {
-  //       setUploadPercentage(100);
-  //       setTimeout(() => {
-  //         setUploadPercentage(0);
-  //       }, 1000);
-  //       setDownloadContent(response.data);
-  //       console.log(response);
-  //       setShowDownload(true);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //       setUploadPercentage(0);
-  //     });
 
   return (
     <div className="fileUpload">
@@ -156,15 +131,17 @@ const FileUpload = () => {
         )}
       </div>
       <RingLoader color={color} loading={loading} css={override} size={150} />
-
-
       {customize ? (
         <>
-          {showCustomizeButton ? (<Button
-            title={"Customize"}
-            classId={buttonId}
-            clickFunc={() => handleCustomize()}
-          ></Button>) : ""}
+          {showCustomizeButton ? (
+            <Button
+              title={"Customize"}
+              classId={buttonId}
+              clickFunc={() => handleCustomize()}
+            ></Button>
+          ) : (
+            ""
+          )}
 
           {validJSON ? (
             <Modal
@@ -179,9 +156,6 @@ const FileUpload = () => {
       ) : (
         <p></p>
       )}
-
-
-
     </div>
   );
 };
