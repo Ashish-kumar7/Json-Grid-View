0294e6a1b2001e8e5aa6cb4767b35c1e9f1a4aba
ashish007kumar <ashishkumar357ak@gmail.com>
2021-06-28 09:20:56 +0530

    updated App.js from version3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,38 @@
-import { Route } from 'react-router-dom';
-import HomePage from './pages/homepage/HomePage';
-import OptionsPage from './pages/optionpage/OptionsPage';
-import FileUpload from './pages/fileupload/FileUpload';
-import Footer from './components/footer/Footer'
-import FileUrl from './pages/fileurl/FileUrl'
-import JsonInput from './pages/jsoninput/JsonInput'
-import SelectedValues from './components/selectedvaluespreview/SelectedValues';
-import JsonChecker from './pages/jsonchecker/JsonChecker';
-import NewPreview from './pages/newpreview/NewPreviewPage';
+  
+import { Route } from "react-router-dom";
+import HomePage from "./pages/homepage/HomePage";
+import OptionsPage from "./pages/optionpage/OptionsPage";
+import FileUpload from "./pages/fileupload/FileUpload";
+import FileUrl from "./pages/fileurl/FileUrl";
+import JsonInput from "./pages/jsoninput/JsonInput";
+import JsonChecker from "./pages/jsonchecker/JsonChecker";
+import NewPreview from "./pages/newpreview/NewPreviewPage";
 
 function App() {
   return (
     <div>
-        <Route exact path="/" ><HomePage /></Route>
-        <Route path="/options"><OptionsPage/></Route>
-        <Route path="/file-upload"><FileUpload /></Route>
-        <Route path="/file-url"><FileUrl /></Route>
-        <Route path="/json-input"><JsonInput /></Route>
-        <Route path="/jsonchecker"><JsonChecker /></Route>
-        <Route path='/newpreview'><NewPreview /></Route>
-        <Route path='/footer'><Footer /></Route>
-        <Route path='/selected'><SelectedValues /></Route>
+      <Route path="/jsonchecker">
+        <JsonChecker />
+      </Route>
+      <Route exact path="/">
+        <HomePage />
+      </Route>
+      <Route path="/options">
+        <OptionsPage />
+      </Route>
+      <Route path="/file-upload">
+        <FileUpload />
+      </Route>
+      <Route path="/file-url">
+        <FileUrl />
+      </Route>
+      <Route path="/json-input">
+        <JsonInput />
+      </Route>
+      <Route path="/newpreview">
+        <NewPreview></NewPreview>
+      </Route>
     </div>
-
   );
 }
 
