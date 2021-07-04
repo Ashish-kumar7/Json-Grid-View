fa9ff080602467f6512a281cfaf361b48f8d8eb3
Ashish Kumar <ashishkumar357ak@gmail.com>
2021-07-04 09:06:25 +0530

    added .bat files for running hadoop server, backend (globally as well as virtual env).

---
+++ backend/runBackend.bat
@@ -0,0 +1,11 @@
+@echo off
+title Starting Json-Grid-View/backend
+
+echo Starting backend ...
+
+cd E:\DBInternProject\InternProject\Json-Grid-View\backend
+echo The current directory is %CD%
+
+call python "App.py"
+
+pause
